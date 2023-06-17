3f67ab7122bce4494992a9bf5717c8aaaf31a582
sanjith <sanjith1309@gmail.com>
2023-06-17 16:45:35 +0530

    by sanjith

--- app/src/main/java/com/example/projectpdf/uploadpdf.java
+++ app/src/main/java/com/example/projectpdf/uploadpdf.java
@@ -1,15 +1,41 @@
 package com.example.projectpdf;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import android.provider.OpenableColumns;
+
+import java.io.File;
 
 public class uploadpdf extends AppCompatActivity {
 
     String selectedtext;
+    Button uploadBTn;
     TextView tx;
+    EditText edit;
+    StorageReference storageReference;
+    DatabaseReference databaseReference;
 
 
     @Override
@@ -17,13 +43,128 @@ public class uploadpdf extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uploadpdf);
         tx = (TextView) findViewById(R.id.textshow);
+        edit = findViewById(R.id.editTextText);
+        uploadBTn = findViewById(R.id.button4);
 
 
         selectedtext = getIntent().getStringExtra("selectedsub_to_upload");
         tx.setText(selectedtext);
 
+        StorageReference storageRef = FirebaseStorage.getInstance().getReferenceFromUrl("gs://projectpdf-9cd03.appspot.com");
+        storageReference = storageRef.child("uploads");
+
+        databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://projectpdf-9cd03-default-rtdb.firebaseio.com/").child(selectedtext);
+        uploadBTn.setEnabled(false);
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectPDF();
+            }
+        });
+    }
+
+
+    private void selectPDF() {
+        Intent intent = new Intent();
+        intent.setType("application/pdf");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Pdf files"),101);
+    }
 
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 101 && resultCode == RESULT_OK && data != null && data.getData() != null){
+            Uri uri =data.getData();
+
+            //we need the file name of the pdf file, so extract the name of the pdf file
+            String uriString  = uri.toString();
+            File myFile = new File(uriString);
+            String path = myFile.getAbsolutePath();
+            String displayName = null;
+
+            if (uriString.startsWith("content://")){
+                Cursor cursor = null;
+                try {
+                    cursor = this.getContentResolver().query(uri,null,null,null,null);
+                    if (cursor != null && cursor.moveToFirst()){
+                        displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                    }
+                } finally {
+                    cursor.close();
+                }
+            } else  if (uriString.startsWith("file://")){
+                displayName = myFile.getName();
+            }
+
+            uploadBTn.setEnabled(true);
+            edit.setText(displayName);
+
+            uploadBTn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    uploadPDF(data.getData());
+                }
+            });
+
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+    private void uploadPDF(Uri data) {
+        final ProgressDialog pd = new ProgressDialog(this);
+        pd.setTitle("File Uploading..");
+        pd.show();
+
+        final StorageReference reference = storageReference.child("uploads/"+ System.currentTimeMillis() + ".pdf");
+        // store in upload folder of the Firebase storage
+        reference.putFile(data)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                        while (!uriTask.isComplete());
+                        Uri uri = uriTask.getResult();
+
+                        FileinModel fileinModel = new FileinModel(edit.getText().toString(), uri.toString()); //get the views from the model class
+                        databaseReference.child(databaseReference.push().getKey()).setValue(fileinModel);// push the value into the realtime database
+                        Toast.makeText(uploadpdf.this, "File Uploaded Successfully!!", Toast.LENGTH_SHORT).show();
+                        pd.dismiss();
+
+                    }
+                }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                        float percent = (100 * snapshot.getBytesTransferred())/ snapshot.getTotalByteCount();
+                        pd.setMessage("Uploaded : "+ (int) percent + "%");
+                    }
+                });
+
+    }
+
+    //lets try and upload pdf file
+    //before
+
+    public void retrievePDFs(View view) {
+        // now here we will extract those pdf files
+        startActivity(new Intent(uploadpdf.this, yearSelect.class));
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
